2e2cc2bdfe1873805de54b00be5d1eb53f24d154
alkrona <alkrona117@gmail.com>
2024-02-01 11:13:16 +0530

    all logic except final validation complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { useState } from "react";
 import Die from "./components/Die";
 
+import dies from "./components/dies";
 function App() {
   const dieValues_temp = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const [dieValues, setDieValues] = useState(dieValues_temp.map(() => {
-    return Math.floor(Math.random() * (9 - 1 + 1)) + 1;
-  }));
+  const [dieValues, setDieValues] = useState(dies)
+  function resetAll(){
+    setDieValues(prevValue=>{
+      return prevValue.map(currentValue=>{
+        return {...currentValue,value:Math.floor(Math.random() * (9 - 1 + 1)) + 1,isOn:false}
+      })
+    })
+  }
+  function handleResetButton(props){
+    console.log("handle reset button working")
+    setDieValues(prevValue => {
+      return prevValue.map(currentValue => {
+        return currentValue.isOn ? currentValue:{...currentValue, value:Math.floor(Math.random() * (9 - 1 + 1)) + 1}
+      });
+    });
+  }
+  function handleClick(id) {
+    
+    setDieValues(prevValue => {
+      return prevValue.map(currentValue => {
+        return currentValue.id === id ? {...currentValue, isOn:!currentValue.isOn}: currentValue
+      });
+    });
+    
+  }
+  
+
 
   return (
     <div className="App">
@@ -16,12 +41,12 @@ function App() {
       
     <div className="DieArray">
       {dieValues.map(die_val => {
-        return <Die value={die_val} key={die_val} />;
+        return <Die value={die_val.value} id = {die_val.id} isOn={die_val.isOn} key={die_val.id} handleClick={handleClick} />;
       })}
     </div>
   
       <div className="ResetButton">
-      <button>Roll</button>
+      <button onClick={handleResetButton}>Roll</button>
       </div>
     </div>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,11 @@
 import React from "react";
-export default function Die(prop){
+export default function Die(props){
+    const styles = {
+        backgroundColor: props.isOn ?  "#59E391":"#FFFFFF" 
+    }
     return(
         <div className="Die">
-        <button>{prop.value}</button>
+        <button style={styles} onClick={()=>props.handleClick(props.id)}>{props.value}</button>
         </div>
     )
 }
